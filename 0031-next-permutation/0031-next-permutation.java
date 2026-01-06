448ef27025e265c8a7e96d5872befd1d7e5b401d
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-01-06 22:35:01 +0530

    Time: 0 ms (100%), Space: 44.8 MB (31.44%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,35 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        int i = n - 2;
-
-        // Step 1: find pivot
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-
-        // Step 2: find successor
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
+        
+        int pivot = -1;
+        for(int i = n - 2; i >= 0; i--) {
+            if(nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
             }
-            swap(nums, i, j);
         }
 
-        // Step 3: reverse suffix
-        reverse(nums, i + 1, n - 1);
+        // Case: no next permutation
+        if(pivot == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        // Find rightmost element greater than pivot
+        for(int i = n - 1; i > pivot; i--) {
+            if(nums[i] > nums[pivot]) {
+                swap(nums, pivot, i);
+                break;
+            }
+        }
+
+        // Reverse suffix
+        reverse(nums, pivot + 1, n - 1);
     }
 
-    void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
 
-    void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start++, end--);
+    public void reverse(int[] nums, int l, int r) {
+        while(l < r) {
+            swap(nums, l, r);
+            l++;
+            r--;
         }
     }
 }
