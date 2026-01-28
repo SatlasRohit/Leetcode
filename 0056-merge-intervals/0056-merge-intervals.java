254b82d1de21c6da1dadf638ff71c92725723fc0
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-01-28 08:28:52 -0800

    Time: 8 ms (89.87%), Space: 49.6 MB (9.86%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,38 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
 
-        if (intervals.length <= 1)
-            return intervals;
+        if (intervals.length == 0) return intervals;
 
-        // Faster than lambda comparator
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        int n = intervals.length;
-        int[][] res = new int[n][2];
-        int idx = 0;
+        ArrayList<int[]> merge = new ArrayList<>();
 
-        res[0][0] = intervals[0][0];
-        res[0][1] = intervals[0][1];
+        int start = intervals[0][0];
+        int end   = intervals[0][1];
 
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] <= res[idx][1]) {
-                // merge
-                if (intervals[i][1] > res[idx][1]) {
-                    res[idx][1] = intervals[i][1];
-                }
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] <= end) {
+                end = Math.max(end, intervals[i][1]);
             } else {
-                idx++;
-                res[idx][0] = intervals[i][0];
-                res[idx][1] = intervals[i][1];
+                merge.add(new int[]{start, end});
+                start = intervals[i][0];
+                end   = intervals[i][1];
             }
         }
 
-        return Arrays.copyOf(res, idx + 1);
+        merge.add(new int[]{start, end});
+
+        return merge.toArray(new int[merge.size()][]);
     }
 }
