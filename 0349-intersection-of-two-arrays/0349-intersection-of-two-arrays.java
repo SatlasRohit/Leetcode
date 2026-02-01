95e3547bd6183ecd029f74b2b5326c5855d196d8
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-01 11:38:27 +0530

    Time: 11 ms (8.84%), Space: 45.5 MB (10.73%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -4,35 +4,61 @@ class Solution {
             if(nums1[0]==nums2[0]){
                 return nums1;
             }
+            else{
+                return new int[0];
+            }
         }
-        HashSet<Integer> Intersection =new HashSet<>();
+        HashSet<Integer> Intersection=new HashSet<>();
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
         if(nums1.length<=nums2.length){
+            int mid=nums2.length/2;
             for(int i=0;i<nums1.length;i++){
-                for(int j=0;j<nums2.length;j++){
-                    if(nums1[i]==nums2[j]){
-                        Intersection.add(nums1[i]);
-                        break;
+                if(nums1[i]>=nums2[mid]){
+                    for(int j=mid;j<nums2.length;j++){
+                        if(nums1[i]==nums2[j]){
+                            Intersection.add(nums1[i]);
+                            break;
+                        }
+                    }
+                }
+                else if(nums1[i]<nums2[mid]){
+                    for(int j=0;j<mid;j++){
+                        if(nums1[i]==nums2[j]){
+                            Intersection.add(nums1[i]);
+                            break;
+                        }
                     }
                 }
             }
         }
         else if(nums2.length<nums1.length){
+            int mid=nums1.length/2;
             for(int i=0;i<nums2.length;i++){
-                for(int j=0;j<nums1.length;j++){
-                    if(nums2[i]==nums1[j]){
-                        Intersection.add(nums2[i]);
-                        break;
+                if(nums2[i]>=nums1[mid]){
+                    for(int j=mid;j<nums1.length;j++){
+                        if(nums2[i]==nums1[j]){
+                            Intersection.add(nums2[i]);
+                            break;
+                        }
+                    }
+                }
+                else if(nums2[i]<nums1[mid]){
+                    for(int j=0;j<mid;j++){
+                        if(nums2[i]==nums1[j]){
+                            Intersection.add(nums2[i]);
+                            break;
+                        }
                     }
                 }
             }
         }
-        
-        int[] intersect=new int[Intersection.size()];
-        int index=0;
-        for(int val:Intersection){
-            intersect[index]=val;
-            index++;
+        int[] arr=new int[Intersection.size()];
+        int i=0;
+        for(int a:Intersection){
+            arr[i]=a;
+            i++;
         }
-        return intersect;
+        return arr;
     }
 }
