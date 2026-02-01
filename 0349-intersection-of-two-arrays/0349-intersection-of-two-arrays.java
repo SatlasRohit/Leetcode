7e8766fcbdc137165da5986629fb2840bb53bbd5
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-01 11:18:17 +0530

    Time: 3 ms (54.49%), Space: 44.8 MB (84.86%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,14 +1,32 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1.length==1 && nums2.length==1){
+            if(nums1[0]==nums2[0]){
+                return nums1;
+            }
+        }
         HashSet<Integer> Intersection =new HashSet<>();
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    Intersection.add(nums1[i]);
-                    break;
+        if(nums1.length<=nums2.length){
+            for(int i=0;i<nums1.length;i++){
+                for(int j=0;j<nums2.length;j++){
+                    if(nums1[i]==nums2[j]){
+                        Intersection.add(nums1[i]);
+                        break;
+                    }
                 }
             }
         }
+        else if(nums2.length<nums1.length){
+            for(int i=0;i<nums2.length;i++){
+                for(int j=0;j<nums1.length;j++){
+                    if(nums2[i]==nums1[j]){
+                        Intersection.add(nums2[i]);
+                        break;
+                    }
+                }
+            }
+        }
+        
         int[] intersect=new int[Intersection.size()];
         int index=0;
         for(int val:Intersection){
