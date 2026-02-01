6a6f4e2d98bfed3555635e6a54e4173d3e436feb
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-01 14:49:32 +0530

    Time: 2 ms (97.05%), Space: 45.2 MB (35.46%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,38 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        if(nums1.length==1 && nums2.length==1){
-            if(nums1[0]==nums2[0]){
-                return nums1;
+
+        // Step 1: Store elements of nums1 in a HashSet
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        // Step 2: Find common elements using another HashSet
+        HashSet<Integer> resultSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num); // ensures uniqueness
             }
         }
-        HashSet<Integer> Intersection =new HashSet<>();
-        if(nums1.length<=nums2.length){
-            for(int i=0;i<nums1.length;i++){
-                for(int j=0;j<nums2.length;j++){
-                    if(nums1[i]==nums2[j]){
-                        Intersection.add(nums1[i]);
-                        break;
-                    }
-                }
-            }
+
+        // Step 3: Convert result HashSet to array
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
         }
-        else if(nums2.length<nums1.length){
-            for(int i=0;i<nums2.length;i++){
-                for(int j=0;j<nums1.length;j++){
-                    if(nums2[i]==nums1[j]){
-                        Intersection.add(nums2[i]);
-                        break;
-                    }
-                }
-            }
-        }
-        
-        int[] intersect=new int[Intersection.size()];
-        int index=0;
-        for(int val:Intersection){
-            intersect[index]=val;
-            index++;
-        }
-        return intersect;
+
+        return result;
     }
 }
