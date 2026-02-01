b9ca77ec823389ef1c51937db75dfe555aa75c44
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-01 15:46:49 +0530

    Time: 0 ms (100%), Space: 44.6 MB (90.23%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,31 +1,28 @@
-import java.util.*;
-
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
-        // Ensure nums1 is the smaller array
-        if (nums1.length > nums2.length) {
-            return intersection(nums2, nums1);
+        boolean[] seen = new boolean[1001];
+
+        // mark nums1 values
+        for (int x : nums1) {
+            seen[x] = true;
         }
 
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums1) {
-            set.add(num);
-        }
+        // collect result (max size = min of arrays)
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int k = 0;
 
-        HashSet<Integer> result = new HashSet<>();
-        for (int num : nums2) {
-            if (set.contains(num)) {
-                result.add(num);
+        for (int x : nums2) {
+            if (seen[x]) {
+                temp[k++] = x;
+                seen[x] = false; // avoid duplicates
             }
         }
 
-        int[] ans = new int[result.size()];
-        int i = 0;
-        for (int num : result) {
-            ans[i++] = num;
-        }
+        // copy exact size result
+        int[] res = new int[k];
+        System.arraycopy(temp, 0, res, 0, k);
 
-        return ans;
+        return res;
     }
 }
