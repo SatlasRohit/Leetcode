f1e9512b255c8084e0514a0729d17d6503bb6b19
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-02 23:13:48 +0530

    Time: 0 ms (100%), Space: 44.9 MB (74.6%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,18 +1,28 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        boolean[] seen=new boolean[1001];
-        for(int a:nums2){
-            seen[a]=true;
+
+        int[] freq = new int[1001];
+
+        // count frequency of nums1
+        for (int x : nums1) {
+            freq[x]++;
         }
-        int[] temp=new int[Math.max(nums1.length,nums2.length)];
-        int k=0;
-        for(int a:nums1){
-            if(seen[a]){
-                temp[k++]=a;
+
+        int[] temp = new int[Math.min(nums1.length, nums2.length)];
+        int k = 0;
+
+        // collect intersection using nums2
+        for (int x : nums2) {
+            if (freq[x] > 0) {
+                temp[k++] = x;
+                freq[x]--;
             }
         }
-        int[] intersect=new int[k];
-        System.arraycopy(temp,0,intersect,0,k);
-        return intersect;
+
+        // copy exact result size
+        int[] res = new int[k];
+        System.arraycopy(temp, 0, res, 0, k);
+
+        return res;
     }
 }
