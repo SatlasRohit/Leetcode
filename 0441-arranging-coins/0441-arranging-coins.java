a9abaf37d63f50afa14522d71e779bbaa7ffd46a
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-03 20:31:58 +0530

    Time: 1 ms (99.95%), Space: 43 MB (19.65%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,6 +1,19 @@
 class Solution {
     public int arrangeCoins(int n) {
-        long N = n;
-        return (int)((Math.sqrt(8 * N + 1) - 1) / 2);
+        long left=1,right=n,ans=0;
+        while(left<=right){
+            long mid=left+(right-left)/2;
+            if(check(n,mid)){
+                left=mid+1;
+                ans=mid;
+            }else{
+                right=mid-1;
+            }
+        }
+        return (int)ans;
     }
+    public boolean check(int n, long m){
+        return (m*(m+1)/2)<=n;
+    }
+    
 }
