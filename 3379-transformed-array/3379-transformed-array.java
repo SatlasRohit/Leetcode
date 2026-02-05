1fb5bfba241904bf896a76785ea74c91cb5ca6d0
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-06 05:12:34 +0530

    Time: 1 ms (98.74%), Space: 46.9 MB (13.84%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.java
+++ 3379-transformed-array/3379-transformed-array.java
@@ -1,19 +1,10 @@
 class Solution {
     public int[] constructTransformedArray(int[] nums) {
-        int len = nums.length;
-        int[] result = new int[len];
-
-        for(int i = 0; i < len; i++) {
-            int index = (i + nums[i]) % len;
-
-            // handle negative index
-            if(index < 0) {
-                index += len;
-            }
-
-            result[i] = nums[index];
-        }
-
-        return result;
-    }
+		int n = nums.length;
+		int[] ans = new int[n];
+		for (int i = 0; i < n; i++) {
+			ans[i] = nums[((i + nums[i]) % n + n) % n];
+		}
+		return ans;
+	}
 }
