8ba9b66d271ea6f199c4ee305c08f57476cfa8db
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-02-20 19:03:21 +0530

    Time: 5 ms (29.4%), Space: 44.1 MB (22.06%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,24 +1,17 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        int j = 0;
-
-        // Place arr2 elements first
-        for(int i = 0; i < arr2.length; i++){
-            for(int l = j; l < arr1.length; l++){
-                if(arr1[l] == arr2[i]){
-                    int temp = arr1[j];
-                    arr1[j] = arr1[l];
-                    arr1[l] = temp;
+        int j=0;
+        for(int i=0;i<arr2.length;i++){
+            for(int l=0;l<arr1.length;l++){
+                if(arr1[l]==arr2[i]){
+                    int temp=arr1[j];
+                    arr1[j]=arr1[l];
+                    arr1[l]=temp;
                     j++;
                 }
             }
         }
-
-        // Sort remaining elements
         Arrays.sort(arr1, j, arr1.length);
-
         return arr1;
     }
 }
