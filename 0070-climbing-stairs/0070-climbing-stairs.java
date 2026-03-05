93c30de05cc3ce1437132cc077e5ec160a487698
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-03-05 22:33:35 +0530

    Time: 0 ms (100%), Space: 42 MB (73.2%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        if(n==0 || n==1){
+            return n;
         }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
+        int prev=1,curr=01;
+        for(int i=2;i<=n;i++){
+            int temp=curr;
+            curr=prev+curr;
+            prev=temp;
         }
         return curr;
     }
