d7f0ab9931d1db05b5c56400f804f41a07ffc280
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-03-06 23:33:32 +0530

    Time: 1617 ms (5.26%), Space: 59.8 MB (5.08%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,14 +4,30 @@ class Solution {
         char[] str2=text2.toCharArray();
         int m=text1.length();
         int n=text2.length();
+        char[][] print=new char[m][n];
         int[][] dp=new int[m+1][n+1];
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(str1[i-1]==str2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
+                    print[i-1][j-1]='d';
                 }
                 else{
                     dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                    int max=Math.max(dp[i][j-1],dp[i-1][j]);
+                    if(max==dp[i][j-1]){
+                        print[i-1][j-1]='r';
+                    }
+                    else{
+                        print[i-1][j-1]='u';
+                    }
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(print[i][j]=='d'){
+                    System.out.print(str1[i]);
                 }
             }
         }
