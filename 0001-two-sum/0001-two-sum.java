fedadf21a9bbd5cddd042c556f7489fc7bd9ccaf
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-03-10 05:04:20 +0530

    Time: 1 ms (99.28%), Space: 46.9 MB (82.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,44 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
-                }
-            }
+        int capacity = 1;
+        while (capacity < nums.length * 2) {
+            capacity <<= 1;
         }
-        return new int[]{};
+        
+        int[] keys = new int[capacity];
+        int[] values = new int[capacity];
+        boolean[] occupied = new boolean[capacity]; // Отслеживаем занятые ячейки
+        
+        int mask = capacity - 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            // Вычисляем индекс с обработкой отрицательных чисел
+            int hash = complement;
+            int idx = (hash & 0x7FFFFFFF) % capacity; // Гарантируем положительный индекс
+            
+            // Поиск complement
+            while (occupied[idx]) {
+                if (keys[idx] == complement) {
+                    return new int[] { values[idx], i };
+                }
+                idx = (idx + 1) & mask;
+            }
+            
+            // Вставка текущего числа
+            int currHash = nums[i];
+            int currIdx = (currHash & 0x7FFFFFFF) % capacity;
+            
+            while (occupied[currIdx]) {
+                currIdx = (currIdx + 1) & mask;
+            }
+            
+            keys[currIdx] = nums[i];
+            values[currIdx] = i;
+            occupied[currIdx] = true;
+        }
+        
+        return new int[0];
     }
 }
