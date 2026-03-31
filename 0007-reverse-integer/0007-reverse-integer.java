aab97ddc6690f360f2984d4f3058fa1739053628
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-03-31 20:26:59 +0530

    Time: 1 ms (99.95%), Space: 42.9 MB (20%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -4,9 +4,9 @@ class Solution {
         while(x!=0){
             rev=((rev*10)+(x%10));
             x/=10;
-            if(rev>=Integer.MAX_VALUE || rev<=Integer.MIN_VALUE){
-                return 0;
-            }
+        }
+        if(rev>=Integer.MAX_VALUE || rev<=Integer.MIN_VALUE){
+            return 0;
         }
         return (int)rev;
     }
