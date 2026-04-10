b93e845c3e8114b76aae1d96cde10fe5132d06d1
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-04-10 21:13:29 +0530

    Time: 2 ms (12.22%), Space: 47.2 MB (13.15%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int[][] temp=new int[mat.length][mat[0].length];
+        int matlen1=mat.length;
+        int matlen2=mat[0].length;
+        for(int i=0;i<matlen1;i++){
+            for(int j=0;j<matlen2;j++){
+                temp[i][j]=mat[i][j];
+            }
+        }
+        for(int i=0;i<k;i++){
+            for(int j=0;j<matlen1;j++){
+                if(j%2!=0){
+                    rightshift(temp,j,matlen2);
+                }
+                else{
+                    leftshift(temp,j,matlen2);
+                }
+            }
+        }
+        for(int i=0;i<matlen1;i++){
+            for(int j=0;j<matlen2;j++){
+                if(mat[i][j]==temp[i][j]) continue;
+                else return false;
+            }
+        }
+        return true;
+    }
+
+    void leftshift(int[][] mat,int j,int matlen2){
+        int temp=mat[j][0];
+        for(int i=0;i<matlen2-1;i++){
+            mat[j][i]=mat[j][i+1];
+        }
+        mat[j][matlen2-1]=temp;
+    }
+
+    void rightshift(int[][] mat,int j,int matlen2){
+        int temp=mat[j][matlen2-1];
+        for(int i=matlen2-1;i>0;i--){
+            mat[j][i]=mat[j][i-1];
+        }
+        mat[j][0]=temp;
+    }
+}
