849cee55a6b91d186e9b11acc7f714f10bff2735
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-04-13 20:04:30 +0530

    Time: 1 ms (47.94%), Space: 44.8 MB (69.96%) - LeetHub

--- 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.java
@@ -3,10 +3,7 @@ class Solution {
         int min=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==target){
-                int k=Math.abs(i-start);
-                if(min>k){
-                    min=k;
-                }
+                min=Math.min(Math.abs(i-start),min);
             }
         }
         return min;
