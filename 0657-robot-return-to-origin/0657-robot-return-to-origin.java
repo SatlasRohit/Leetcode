5fd77baf1a9491cb6be4baded142e75320ea4e8f
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-04-24 07:55:58 +0530

    Time: 3 ms (99.01%), Space: 46.1 MB (45.84%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,18 +1,9 @@
 class Solution {
-    static {
-        String[] arr = new String[1];
-        for (int i = 0; i < 500; i++){
-            judgeCircle("");
+    public boolean judgeCircle(String moves) {
+        int[] array = new int[26];
+        for (char c : moves.toCharArray()) {
+            array[c - 'A']++;
         }
-    }
-    public static boolean judgeCircle(String moves) {
-        int[] freq = new int[26];
-        char[] move = moves.toCharArray();
-        if(move.length % 2 != 0) return false;
-        for(char ch : move){
-            freq[ch - 'A']++;
-        }
-        if(freq['U' - 'A'] == freq['D' - 'A'] && freq['L' - 'A'] == freq['R' - 'A']) return true;
-        return false;
+        return (array['U' - 'A'] == array['D' - 'A']) && (array['L' - 'A'] == array['R' - 'A']);
     }
 }
