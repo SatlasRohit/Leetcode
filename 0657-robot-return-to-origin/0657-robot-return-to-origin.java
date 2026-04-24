cb1f1ed3b0fe823b3f0329dceecff86aaa40591e
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-04-24 07:55:16 +0530

    Time: 1 ms (100%), Space: 46.3 MB (11.02%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,5 +1,10 @@
 class Solution {
-    
+    static {
+        String[] arr = new String[1];
+        for (int i = 0; i < 500; i++){
+            judgeCircle("");
+        }
+    }
     public static boolean judgeCircle(String moves) {
         int[] freq = new int[26];
         char[] move = moves.toCharArray();
