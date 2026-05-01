4412e0c6267343a6a7750df7e1e1153a8caa48ee
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-05-01 06:05:39 +0530

    Time: 3 ms (100%), Space: 110.7 MB (44.28%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,36 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        int div = grid[0][0] % x;
-        int[] arr = new int[n * m];
-        int k = 0;
-
-        // Step 1: Check remainder + flatten
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] % x != div){
-                    return -1;
-                }
-                arr[k++] = grid[i][j];
+        int m = grid.length , n = grid[0].length;
+        int totalElements = m * n;
+        int[] freq = new int[10001];
+        int rem = grid[0][0] % x;
+        for(int[] row : grid){
+            for(int num : row){
+                if(num % x != rem) return -1;
+                freq[num]++;
             }
         }
-
-        // Step 2: Sort
-        Arrays.sort(arr);
-
-        // Step 3: Find median
-        int median = arr[arr.length / 2];
-
-        // Step 4: Count operations
-        int count = 0;
-        for(int i = 0; i < arr.length; i++){
-            count += Math.abs(arr[i] - median) / x;
+        int middle = (totalElements + 1) / 2;
+        int leftElementCount = 0;
+        for(int i = 1 ; i < freq.length ; i++){
+            if(freq[i] == 0) continue;
+            leftElementCount += freq[i];
+            if(leftElementCount >= middle){
+                middle = i;
+                break;
+            }
         }
-
-        return count;
+        int op = 0;
+        for(int i = 0 ; i < freq.length ; i++){
+            if(freq[i] == 0) continue;
+            op += (Math.abs(i - middle) / x) * freq[i];
+        }
+        return op;
     }
 }
