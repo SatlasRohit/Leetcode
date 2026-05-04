8f1202970c1e18e298735f3de898e7fb3e88cf23
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-05-04 14:23:11 +0530

    Time: 0 ms (100%), Space: 43.7 MB (69.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,7 +1,8 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        for(int i=0;i<matrix.length;i++){
-            for(int j=i;j<matrix.length;j++){
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
                 if(i==j){
                     continue;
                 }
@@ -16,9 +17,9 @@ class Solution {
         //     }
         //     System.out.println("");
         // }
-        for(int i=0;i<matrix.length;i++){
+        for(int i=0;i<n;i++){
             int k=matrix.length-1;
-            for(int j=0;j<matrix.length/2;j++){
+            for(int j=0;j<n/2;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[i][k];
                 matrix[i][k]=temp;
