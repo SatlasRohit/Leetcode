f5246f8bd96e7b30fd0c99d9fd9801720fd8e95c
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-05-13 21:05:50 +0530

    Time: 0 ms (100%), Space: 44.4 MB (91.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,34 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        
-        int pivot = -1;
-        for(int i = n - 2; i >= 0; i--) {
-            if(nums[i] < nums[i + 1]) {
-                pivot = i;
+        int n=nums.length;
+        int pivot=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                pivot=i;
                 break;
             }
         }
-
-        // Case: no next permutation
-        if(pivot == -1) {
-            reverse(nums, 0, n - 1);
+        if(pivot==-1){
+            reverse(nums,0,n-1);
             return;
         }
-
-        // Find rightmost element greater than pivot
-        for(int i = n - 1; i > pivot; i--) {
-            if(nums[i] > nums[pivot]) {
-                swap(nums, pivot, i);
+        for(int i=n-1;i>pivot;i--){
+            if(nums[i]>nums[pivot]){
+                swap(nums,i,pivot);
                 break;
             }
         }
-
-        // Reverse suffix
-        reverse(nums, pivot + 1, n - 1);
+        reverse(nums,pivot+1,n-1);
+    }
+    public void swap(int[] nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    public void reverse(int[] nums, int l, int r) {
-        while(l < r) {
-            swap(nums, l, r);
+    public void reverse(int[] nums,int l,int r){
+        while(l<r){
+            swap(nums,l,r);
             l++;
             r--;
         }
