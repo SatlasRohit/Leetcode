70ff915889c0c2e7403fc7edfa25a521a60754dc
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-05-19 20:23:05 +0530

    Time: 0 ms (100%), Space: 80.4 MB (48.43%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -5,9 +5,15 @@ class Solution {
         int n2=nums2.length;
         if(nums1[n1-1]<nums2[0] || nums2[n2-1]<nums1[0]) return -1;
         while(i < n1 && j < n2){
-            if(nums1[i]==nums2[j]) return nums1[i];
-            else if(nums1[i]<nums2[j]) i++;
-            else j++;
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            } 
+            else if(nums1[i]<nums2[j]){
+                i++;
+            } 
+            else{
+                j++;
+            }
         }
         return -1;
     }
