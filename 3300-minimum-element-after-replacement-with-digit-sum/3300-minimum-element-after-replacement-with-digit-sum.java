058a219bb9d5cba3bc3a44f7ff2261ebdda971b6
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-05-29 18:39:36 +0530

    Time: 1 ms (100%), Space: 44.7 MB (86.9%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.java
@@ -2,13 +2,13 @@ class Solution {
     public int minElement(int[] nums) {
         int min=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++){
-            int digit=nums[i];
+            
             int sum=0;
-            while(digit>9){
-                sum+=digit%10;
-                digit/=10;
+            while(nums[i]>9){
+                sum+=nums[i]%10;
+                nums[i]/=10;
             }
-            sum+=digit;
+            sum+=nums[i];
             if(sum<min) min=sum;
         }
         return min;
