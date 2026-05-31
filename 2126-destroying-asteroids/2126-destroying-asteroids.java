d3529a14089b3fbf3ca43faf7a23b46545a84800
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-05-31 15:41:28 +0530

    Time: 5 ms (99.54%), Space: 105.8 MB (94.92%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,13 +1,22 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        int n=asteroids.length;
-        Arrays.sort(asteroids);
-        long currmass=mass;
-        for(int i : asteroids){
-            if(currmass<i){
-                return false;
-            }
-            currmass+=i;
+        int min = 100000;
+        int max = 0;
+        for(int x : asteroids) {
+            max = Math.max(x, max);
+            min = Math.min(x, min);
+        }
+
+        int[] freq = new int[max + 1];
+        long val = mass;
+        for(int x : asteroids) {
+            if(x > val) freq[x]++;
+            else val += x;
+        }
+        if(val >= max) return true;
+        for(int i = min; i <= max; i++) {
+            if(val < i) return false;
+            if(freq[i] != 0) val += i * freq[i];
         }
         return true;
     }
