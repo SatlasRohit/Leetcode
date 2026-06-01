001f22523d0317fd03f91f1444d5fee16d180e84
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-06-01 19:40:28 +0530

    Time: 1 ms (100%), Space: 44.2 MB (94.99%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -1,10 +1,14 @@
 class Solution {
     public int minimumCost(int[] costs) {
         int[] candies = new int[101];
-        for (int c: costs) candies[c]++;
+        int max=Integer.MIN_VALUE;
+        for (int c: costs){
+            candies[c]++;
+            if(c>max) max=c;
+        }
         int bought = 0;
         int cost = 0;
-        for (int i = 100; i>0; i--) {
+        for (int i = max; i>0; i--) {
             if (candies[i] > 2-bought) {
                 candies[i] -= (3-bought);
                 cost += (2-bought)*i;
