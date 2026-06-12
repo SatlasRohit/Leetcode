d0932b53c17a8d393bd9832a72578d70946c8fb9
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-06-12 19:21:05 +0530

    Time: 131 ms (5.15%), Space: 47.9 MB (7.85%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String str = "";
+        for(int i=0;i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+                str+=s.charAt(i);
+                continue;
+            }
+            if(Character.isLetter(s.charAt(i))){
+                str+=Character.toLowerCase(s.charAt(i));
+            }
+        }
+        System.out.println(str);
+        int strlen = str.length();
+        int left=0,right=strlen-1;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
