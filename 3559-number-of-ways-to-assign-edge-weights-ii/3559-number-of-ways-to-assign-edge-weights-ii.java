09d73ada9031533a05b854219a0333ada1e103a2
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-06-12 17:24:25 +0530

    Time: 78 ms (95.65%), Space: 276.4 MB (86.96%) - LeetHub

--- 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.java
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.java
@@ -1,113 +1,114 @@
-import java.util.*;
-
 class Solution {
-
-    static final int MOD = 1_000_000_007;
-    int LOG;
-    int[][] up;
-    int[] depth;
-    List<Integer>[] graph;
+    static final long MOD = 1_000_000_007L;
 
     public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
-
         int n = edges.length + 1;
 
-        graph = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
+        int[] deg = new int[n + 1];
         for (int[] e : edges) {
-            int u = e[0];
-            int v = e[1];
-
-            graph[u].add(v);
-            graph[v].add(u);
+            deg[e[0]]++;
+            deg[e[1]]++;
         }
 
-        LOG = 17;
+        int[][] g = new int[n + 1][];
+        for (int i = 1; i <= n; i++) {
+            g[i] = new int[deg[i]];
+        }
+
+        int[] idx = new int[n + 1];
+        for (int[] e : edges) {
+            int u = e[0], v = e[1];
+            g[u][idx[u]++] = v;
+            g[v][idx[v]++] = u;
+        }
+
+        int LOG = 1;
         while ((1 << LOG) <= n) LOG++;
 
-        up = new int[n + 1][LOG];
-        depth = new int[n + 1];
+        int[][] up = new int[LOG][n + 1];
+        int[] depth = new int[n + 1];
 
-        dfs(1, 0);
+        int[] stack = new int[n];
+        int top = 0;
+        stack[top++] = 1;
+        boolean[] vis = new boolean[n + 1];
+        vis[1] = true;
 
-        for (int j = 1; j < LOG; j++) {
-            for (int i = 1; i <= n; i++) {
-                up[i][j] = up[up[i][j - 1]][j - 1];
+        while (top > 0) {
+            int u = stack[--top];
+
+            for (int v : g[u]) {
+                if (!vis[v]) {
+                    vis[v] = true;
+                    depth[v] = depth[u] + 1;
+                    up[0][v] = u;
+                    stack[top++] = v;
+                }
             }
         }
 
-        int maxDepth = n;
-        long[] pow2 = new long[maxDepth + 1];
-        pow2[0] = 1;
+        for (int k = 1; k < LOG; k++) {
+            for (int v = 1; v <= n; v++) {
+                up[k][v] = up[k - 1][up[k - 1][v]];
+            }
+        }
 
-        for (int i = 1; i <= maxDepth; i++) {
+        int maxLen = 0;
+        for (int[] q : queries) {
+            int u = q[0], v = q[1];
+            int lca = lca(u, v, up, depth, LOG);
+            maxLen = Math.max(maxLen, depth[u] + depth[v] - 2 * depth[lca]);
+        }
+
+        long[] pow2 = new long[Math.max(1, maxLen + 1)];
+        pow2[0] = 1;
+        for (int i = 1; i <= maxLen; i++) {
             pow2[i] = (pow2[i - 1] * 2) % MOD;
         }
 
         int[] ans = new int[queries.length];
 
         for (int i = 0; i < queries.length; i++) {
-
             int u = queries[i][0];
             int v = queries[i][1];
 
-            int lca = getLCA(u, v);
+            int lca = lca(u, v, up, depth, LOG);
+            int len = depth[u] + depth[v] - 2 * depth[lca];
 
-            int dist = depth[u] + depth[v] - 2 * depth[lca];
-
-            if (dist == 0) {
+            if (len == 0) {
                 ans[i] = 0;
             } else {
-                ans[i] = (int) pow2[dist - 1];
+                ans[i] = (int) pow2[len - 1];
             }
         }
 
         return ans;
     }
 
-    private void dfs(int node, int parent) {
-
-        up[node][0] = parent;
-
-        for (int next : graph[node]) {
-
-            if (next == parent) continue;
-
-            depth[next] = depth[node] + 1;
-
-            dfs(next, node);
-        }
-    }
-
-    private int getLCA(int a, int b) {
-
+    private int lca(int a, int b, int[][] up, int[] depth, int LOG) {
         if (depth[a] < depth[b]) {
-            int temp = a;
+            int t = a;
             a = b;
-            b = temp;
+            b = t;
         }
 
         int diff = depth[a] - depth[b];
 
-        for (int j = LOG - 1; j >= 0; j--) {
-            if ((diff & (1 << j)) != 0) {
-                a = up[a][j];
+        for (int k = 0; k < LOG; k++) {
+            if (((diff >> k) & 1) != 0) {
+                a = up[k][a];
             }
         }
 
         if (a == b) return a;
 
-        for (int j = LOG - 1; j >= 0; j--) {
-
-            if (up[a][j] != up[b][j]) {
-                a = up[a][j];
-                b = up[b][j];
+        for (int k = LOG - 1; k >= 0; k--) {
+            if (up[k][a] != up[k][b]) {
+                a = up[k][a];
+                b = up[k][b];
             }
         }
 
-        return up[a][0];
+        return up[0][a];
     }
 }
