39539e970f057cc77e4707036d8386320aed6205
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-06-14 18:29:28 +0530

    Time: 1 ms (100%), Space: 44 MB (95.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.isEmpty()) {
-        	return true;
-        }
-        int start = 0;
-        int last = s.length() - 1;
-        while(start <= last) {
-        	char currFirst = s.charAt(start);
-        	char currLast = s.charAt(last);
-        	if (!Character.isLetterOrDigit(currFirst )) {
-        		start++;
-        	} else if(!Character.isLetterOrDigit(currLast)) {
-        		last--;
-        	} else {
-        		if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
-        			return false;
-        		}
-        		start++;
-        		last--;
-        	}
+        int i=0,j=s.length()-1;
+        while(i<j){
+            char left = s.charAt(i);
+            char right = s.charAt(j);
+            if(!((left >= 'A' && left <= 'Z') 
+            ||
+            (left >='a' && left <='z')
+            ||
+            (left >= '0' && left <= '9')
+            )){
+                i++;
+                continue;
+            }
+            if(!((right >= 'A' && right <= 'Z') 
+            ||
+            (right >='a' && right <='z')
+            ||
+            (right >= '0' && right <= '9')
+            )){
+                j--;
+                continue;
+            }
+            if(left >= 'A' && left <= 'Z'){
+                left = (char)(left+32);
+            }
+
+            if(right >='A' && right <='Z'){
+                right=(char)(right+32);
+            }
+            
+            if(left!=right){
+                return false;
+            }
+            i++;
+            j--;
+
         }
         return true;
     }
