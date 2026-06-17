d468201dde01594654df99847aa186848ccabd41
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-06-17 11:37:26 +0530

    Time: 4 ms (76.3%), Space: 55.7 MB (20.74%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -1,25 +1,22 @@
 class Solution {
     public String processStr(String s) {
-        int n = s.length();
-        StringBuilder result = new StringBuilder();
-        for(int i =0 ;i<n;i++){
-            char ch =s.charAt(i);
-            if(ch == '*'){
-                int len = result.length();
-                if(len>0){
-                    result.deleteCharAt(len-1);
+        StringBuilder str = new StringBuilder();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '#'){
+                str.append(str.toString());
+            }else if(ch == '%'){
+                str.reverse();
+            }else if(ch == '*'){
+                if(str.length() > 0){
+                    str.deleteCharAt(str.length() - 1);
                 }
-            }
-            else if(ch == '#'){
-                result.append(result.toString());
-            }
-            else if(ch == '%'){
-                result.reverse();
-            }
-            else{
-                result.append(ch);
+            }else{
+                str.append(ch);
             }
         }
-        return result.toString();
+        return str.toString();
+
     }
 }
