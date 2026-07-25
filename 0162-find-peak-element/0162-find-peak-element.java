dac6e0cdc4ac9ef7bf8530b307332c870666328a
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-07-26 00:06:01 +0530

    Time: 0 ms (100%), Space: 44.3 MB (37.26%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-        if(n==1) return 0;
-        else if(n>=2){
-            if(nums[0] > nums[1]) return 0;
-            else if(nums[n-1] > nums[n-2]) return n-1;
-        }
-        for(int i=1;i<n-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
+        int low = 0,high = n-1;
+        while(low < high){
+            int mid = (low+high)/2;
+            if(nums[mid]>nums[mid+1]){
+                high = mid;
+            }
+            else{
+                low = mid+1;
             }
         }
-        return -1;
+        return low;
     }
 }
