c81baafdf82ed402c3c0ae2a49e95ca265cc3d4d
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-07-30 21:43:55 +0530

    Time: 1 ms (99.6%), Space: 42.7 MB (85.44%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,17 +1,104 @@
-class Solution {
-    public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-        for(int i : stones){
-            pq.offer(i);
+public class Solution {
+    public int lastStoneWeight(int[] arr){
+        MaxHeap maxHeap = new MaxHeap();
+        for(int i=0; i<arr.length;i ++){
+            maxHeap.add(arr[i]);
         }
-        while(pq.size() > 1){
-            int i = pq.poll(); // 1st largest
-            int j = pq.poll(); // 2nd largest
-
-            if(i != j){
-                pq.offer(i-j);
+        while(maxHeap.getSize()>1){
+            int max = maxHeap.extractMax();
+            int secondMax = maxHeap.extractMax();
+            int newWeight = max-secondMax;
+            if(newWeight!=0){
+                maxHeap.add(newWeight);
             }
         }
-        return pq.size() == 0 ? 0 : pq.poll();
+        if(maxHeap.getSize()==0){
+            return 0;
+        }
+    return maxHeap.getRootElement();
     }
+
+private class MaxHeap {
+    private int capacity = 5;
+    private int[] array = new int[capacity];
+    private int size=0;
+
+    public void add(int x){
+        if(size==capacity){
+            resize();
+        }
+        array[size]=x;
+        size++;
+        heapifyUp(size-1);
+    }
+
+    public int extractMax(){
+        int max = array[0];
+        array[0] = array[size-1];
+        size--;
+        heapifyDown(0);
+        return max;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public int getRootElement(){
+        return array[0];
+    }
+
+    // O(n)
+    private void resize(){
+        capacity = capacity + capacity/2;
+        int[] newArray = new int[capacity];
+        for(int i=0; i<size; i++){
+            newArray[i] = array[i];
+        }
+        array = newArray;
+    }
+
+    // O(log n)
+    private void heapifyUp(int index){
+        while(true){
+            int parent = (index-1)/2;
+            if(array[parent]<array[index]){
+                swap(parent,index);
+                index=parent;
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    private void swap(int index1, int index2){
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    // O(log n)
+    private void heapifyDown(int parent){
+        while(true){
+            int left = 2*parent+1;
+            int right = 2*parent+2;
+            
+            int maxIndex = -1;
+            if(right<size && array[right]>array[left] && array[right]>array[parent]){
+                maxIndex = right;
+            }
+            else if(left<size && array[left]>=array[right] && array[left]>array[parent]){
+                maxIndex = left;
+            }
+            if(maxIndex==-1)
+                break;
+            else{
+                swap(maxIndex,parent);
+                parent = maxIndex;
+            }
+        }
+    }
+}
+
 }
