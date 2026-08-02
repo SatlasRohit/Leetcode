0f1fb2afdbeb5bb8c19df6d1ef43504363281c8b
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-08-02 14:37:02 +0530

    Time: 0 ms (100%), Space: 43.1 MB (62.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,17 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        Arrays.sort(piles);
-        int n = piles.length;
-        int alice = 0;
-        int bob = 0;
-        for(int i=0;i<n;i++){
-            if(i%2 ==0){
-                bob +=piles[i];
-            }
-            else{
-                alice +=piles[i];
-            }
-        }
-        return (alice>bob);
+        return true;
     }
 }
