0b5a477e10c0994286b363e5c8c9dc58cd4d9e60
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-08-04 22:21:33 +0530

    Time: 13 ms (80.38%), Space: 47.7 MB (49.78%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,55 +1,28 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        if(k == nums.length) return nums;
-        HashMap<Integer,Integer> hash = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            hash.put(nums[i],hash.getOrDefault(nums[i],0)+1);
-        }
-        PriorityQueue<Integer> queue = new PriorityQueue<>(
-            (n1,n2) -> hash.get(n1) - hash.get(n2)
-        );
-        for(int n : hash.keySet()){
-            queue.add(n);
-            if(queue.size()>k) queue.poll();
-        }
-        int[] arr = new int[k];
-        for(int i = k-1;i>=0;i--){
-            arr[i]=queue.poll();
-        }
-        return arr;
+class FreqComparator implements Comparator<Map.Entry<Integer,Integer>>{
+    public int compare(Map.Entry<Integer,Integer> a ,Map.Entry<Integer,Integer> b){
+        return b.getValue() - a.getValue();
     }
 }
-// class Solution {
-//     public int[] topKFrequent(int[] nums, int k) {
-//         // O(1) time
-//         if (k == nums.length) {
-//             return nums;
-//         }
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer,Integer> mpp = new HashMap<>();
+        for(int i : nums){
+            mpp.put(i,mpp.getOrDefault(i,0)+1);
+        }
+        // O(N)
+        PriorityQueue<Map.Entry<Integer,Integer>> maxHeap = new PriorityQueue<>(new FreqComparator());
+        int[] result = new int[k];
+
+        for(Map.Entry<Integer,Integer> entry : mpp.entrySet()){
+            maxHeap.offer(entry); // log(N)
+        }
+        // m times log(n);
+        // so O(m log n)
         
-//         // 1. Build hash map: character and how often it appears
-//         // O(N) time
-//         Map<Integer, Integer> count = new HashMap();
-//         for (int n: nums) {
-//           count.put(n, count.getOrDefault(n, 0) + 1);
-//         }
-
-//         // init heap 'the less frequent element first'
-//         Queue<Integer> heap = new PriorityQueue<>(
-//             (n1, n2) -> count.get(n1) - count.get(n2));
-
-//         // 2. Keep k top frequent elements in the heap
-//         // O(N log k) < O(N log N) time
-//         for (int n: count.keySet()) {
-//           heap.add(n);
-//           if (heap.size() > k) heap.poll();    
-//         }
-
-//         // 3. Build an output array
-//         // O(k log k) time
-//         int[] top = new int[k];
-//         for(int i = k - 1; i >= 0; --i) {
-//             top[i] = heap.poll();
-//         }
-//         return top;
-//     }
-// }
+        for(int i=0;i<k;i++){
+            result[i] = maxHeap.poll().getKey();
+}
+        return result;
+    }
+}
+// sp -> O(k)
