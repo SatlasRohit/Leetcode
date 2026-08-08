95c65f74d6c1bcdc52e9950022a1709e0e0e4f64
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-08-08 23:23:31 +0530

    Time: 15 ms (54.78%), Space: 47.6 MB (49.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,15 +1,17 @@
-class Freecomparator implements Comparator<Map.Entry<Integer,Integer>>{
-    public int compare(Map.Entry<Integer,Integer> a , Map.Entry<Integer,Integer> b){
-        return b.getValue() - a.getValue();
-    }
-}
+// class Freecomparator implements Comparator<Map.Entry<Integer,Integer>>{
+//     public int compare(Map.Entry<Integer,Integer> a , Map.Entry<Integer,Integer> b){
+//         return b.getValue() - a.getValue();
+//     }
+// }
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer,Integer> hs = new HashMap<>();
         for(int i : nums){
             hs.put(i,hs.getOrDefault(i,0)+1);
         }
-        PriorityQueue<Map.Entry<Integer,Integer>> pr = new PriorityQueue<>(new Freecomparator());
+        PriorityQueue<Map.Entry<Integer,Integer>> pr = new PriorityQueue<>(
+            (a,b)-> b.getValue() - a.getValue()
+        );
         int[] result = new int[k];
         for(Map.Entry<Integer,Integer> map : hs.entrySet()){
             pr.offer(map);
