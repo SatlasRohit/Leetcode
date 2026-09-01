6e7891585eb6c6bf176ed345d543b0a68c559a6c
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-01 23:39:45 +0530

    Time: 4 ms (100%), Space: 46 MB (52.81%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev = 0,n=x;
+        while(n>0){
+            rev = (rev*10)+(n%10);
+            n/=10;
+        }
+        if(rev == x) return true;
+        return false;
+    }
+}
