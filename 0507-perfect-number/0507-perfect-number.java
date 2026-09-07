353768568f2ba46a80f98c291e6920b3d1152866
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-07 18:18:00 +0530

    Time: 2090 ms (47.22%), Space: 41.7 MB (94.37%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,6 +1,11 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-       return num == 6 ||num ==28 ||num ==496 ||num == 8128 ||num ==33550336;
-       
+        int sum = 0;
+        int temp = num;
+        for(int i=1;i<num;i++){
+            if(num%i == 0) sum +=i;
+        }
+        if(sum != num) return false;
+        return true;
     }
 }
