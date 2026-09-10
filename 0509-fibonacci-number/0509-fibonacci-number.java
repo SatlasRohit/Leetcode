d109cdef5aa874bc5c43456646cb12c20d40eab4
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-10 20:48:44 +0530

    Time: 10 ms (25.81%), Space: 42.3 MB (15.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,7 @@
 class Solution {
-    int fibonacci(int n){
-        if(n==0 || n == 1){
-            return n;
-        }
-        return fibonacci(n-1)+fibonacci(n-2);
-    }
     public int fib(int n) {
-        return fibonacci(n);
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fib(n-1) + fib(n-2);
     }
 }
