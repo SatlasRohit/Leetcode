ea5869c30ad9c080bba74ce980046b2026ef1384
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-10 20:36:54 +0530

    Time: 10 ms (32.9%), Space: 58.6 MB (5.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,42 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i=0,j=s.length()-1;
-        while(i<j){
-            char left = s.charAt(i);
-            char right = s.charAt(j);
-            if(!((left >= 'A' && left <= 'Z') 
-            ||
-            (left >='a' && left <='z')
-            ||
-            (left >= '0' && left <= '9')
-            )){
-                i++;
-                continue;
-            }
-            if(!((right >= 'A' && right <= 'Z') 
-            ||
-            (right >='a' && right <='z')
-            ||
-            (right >= '0' && right <= '9')
-            )){
-                j--;
-                continue;
-            }
-            if(left >= 'A' && left <= 'Z'){
-                left = (char)(left+32);
-            }
-
-            if(right >='A' && right <='Z'){
-                right=(char)(right+32);
-            }
-            
-            if(left!=right){
-                return false;
-            }
-            i++;
-            j--;
-
-        }
-        return true;
+        return Check(s,0,s.length()-1);
+    }
+    public boolean Check(String s,int left,int right){
+        if(left>=right) return true;
+        if(!Character.isLetterOrDigit(s.charAt(left))) return Check(s,left+1,right);
+        else if(!Character.isLetterOrDigit(s.charAt(right))) return Check(s,left,right-1);
+        if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) return false;
+        return Check(s,left+1,right-1);
     }
 }
