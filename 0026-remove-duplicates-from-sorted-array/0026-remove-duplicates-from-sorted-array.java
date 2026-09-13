d993005b1cd5991b645240676d31ad22efa6866d
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-13 22:38:43 +0530

    Time: 1 ms (76.85%), Space: 46.9 MB (24.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -4,7 +4,7 @@ class Solution {
         int j = 0;
         for(int i=1;i<n;i++){
             if(nums[i] == nums[j]) continue; // Continue until the num[i] & nums[j] are equal 
-            nums[++j] = nums[i];
+            nums[++j] = nums[i];//If the nums[i] & nums[j] are not equal then pass value from i to j
         }
         return j+1;
     }
