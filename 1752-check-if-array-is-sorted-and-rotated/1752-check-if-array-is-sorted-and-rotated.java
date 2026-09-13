4f95e9b2f5cd074f8a0c7b2ce71c6be054cba1a8
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-13 22:33:35 +0530

    Time: 0 ms (100%), Space: 43 MB (64.82%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int flag = 0 ;
         for(int i=0;i<n;i++){
-            if(nums[i] > nums[(i+1)%n]) flag++;
+            if(nums[i] > nums[(i+1)%n]) flag++;// If the Array is sorted lesser value comes only once if it is not sorted the lesser value comes more than 1
             if(flag > 1) return false;
         }
         return true;
