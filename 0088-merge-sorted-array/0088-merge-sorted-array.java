f8c9b069b0210c9cbc51ed22d01796480ed504fa
Satlas Rohit <169024948+SatlasRohit@users.noreply.github.com>
2026-09-16 21:18:08 +0530

    Time: 0 ms (100%), Space: 43.5 MB (95.07%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,22 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int[] merge = new int[m + n];
-        int j = 0, k = 0;
+        int i = m - 1;        // Last valid element in nums1
+        int j = n - 1;        // Last element in nums2
+        int k = m + n - 1;    // Last position in nums1
 
-        for (int i = 0; i < merge.length; i++) {
+        while (j >= 0) {
 
-            if (j < m && k < n) {
-                if (nums1[j] < nums2[k]) {
-                    merge[i] = nums1[j++];
-                } else {
-                    merge[i] = nums2[k++];
-                }
-            } else if (j < m) {
-                merge[i] = nums1[j++];
-            } else {
-                merge[i] = nums2[k++];
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } 
+            else {
+                nums1[k] = nums2[j];
+                j--;
             }
-        }
 
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = merge[i];
+            k--;
         }
     }
 }
